9a76b11f3c31d01b08a95593dbb76177823c4158
ibrahimcs80 <32695364+ibrahimcs80@users.noreply.github.com>
2020-03-20 18:34:55 +0200

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+ 
+INSERT INTO PRODUCTS (title, description, price,imageURL,views) VALUES
+                      ('Alfa', ' Product A', 22.0,'/m/image1' ,20),
+                      ('Beta', ' Product B', 50.0,'/m/image2', 30),
+                     ('Gamma', ' Product C', 100.0,'/m/image3',40);
